71dc08ee3acb1942d90210b01680ac484a8961f1
hugooomaciias <06hugo.macias.jimenez04@gmail.com>
2026-08-11 14:03:34 +0200

    Refactor(Router): Refactorized router logic

---
+++ frontend/src/pages/app/main/BasePage.jsx
@@ -0,0 +1,30 @@
+/** React & Third-Party Libraries */
+import { Outlet } from "react-router-dom";
+
+/** Components & Layouts */
+import { NavbarComponent } from "../../../components/app/common/NavbarComponent.jsx";
+
+/**
+ * Main Application Dashboard Component
+ *
+ * This purely visual component acts as the primary layout wrapper for the authenticated area.
+ * It renders the responsive grid layout where widgets are dynamically injected. All data fetching,
+ * state management, and grid modification logic (such as dragging and resizing) are entirely delegated
+ * to its dedicated headless hook (`useHomeLogic`).
+ *
+ * @component
+ * @returns {JSX.Element|null} The rendered dashboard layout, or null if data is not loaded.
+ */
+export const BasePage = () => {
+    return (
+        <div className="flex flex-col md:flex-row h-[100dvh] bg-gradient-to-t md:bg-gradient-to-r from-primary-50 to-primary-300 p-2 md:p-4 gap-4 md:gap-8 overflow-hidden">
+            {/* Vertical Navbar */}
+            <NavbarComponent />
+
+            {/* Main Content Area */}
+            <section className="flex-1 flex flex-col gap-6 w-full h-full overflow-hidden">
+                <Outlet />
+            </section>
+        </div>
+    );
+};
