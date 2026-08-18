674aa379bf7495d3230cc78238ba59102eef82df
hugooomaciias <06hugo.macias.jimenez04@gmail.com>
2026-08-19 00:54:42 +0200

    Fix: Register logic and erro 404 on refresh

--- api/src/main/java/com/tikal/api/service/UserOnboardingService.java
+++ api/src/main/java/com/tikal/api/service/UserOnboardingService.java
@@ -165,7 +165,7 @@ public class UserOnboardingService {
         personalList = projectRepository.save(personalList);
 
         Stage sublist1 = new Stage();
-        sublist1.setName("Trámites");
+        sublist1.setName("Tr�mites");
         sublist1.setProject(personalList);
         sublist1.setColour("y2");
         sublist1 = stageRepository.save(sublist1);
@@ -176,28 +176,32 @@ public class UserOnboardingService {
         sublist2.setColour("b2");
         sublist2 = stageRepository.save(sublist2);
 
+        // Tarea 1: Renovar DNI
         Task listTask1 = new Task();
         listTask1.setName("Renovar el DNI");
         listTask1.setStage(sublist1);
         listTask1.setAssignedUser(user);
         taskRepository.save(listTask1);
 
+        // Tarea 2: Tomate
         Task listTask2 = new Task();
-        listTask1.setName("Tomate");
-        listTask1.setStage(sublist2);
-        listTask1.setAssignedUser(user);
+        listTask2.setName("Tomate");
+        listTask2.setStage(sublist2);
+        listTask2.setAssignedUser(user);
         taskRepository.save(listTask2);
 
+        // Tarea 3: Leche
         Task listTask3 = new Task();
-        listTask1.setName("Leche");
-        listTask1.setStage(sublist2);
-        listTask1.setAssignedUser(user);
+        listTask3.setName("Leche");
+        listTask3.setStage(sublist2);
+        listTask3.setAssignedUser(user);
         taskRepository.save(listTask3);
 
+        // Tarea 4: Pan
         Task listTask4 = new Task();
-        listTask1.setName("Pan");
-        listTask1.setStage(sublist2);
-        listTask1.setAssignedUser(user);
+        listTask4.setName("Pan");
+        listTask4.setStage(sublist2);
+        listTask4.setAssignedUser(user);
         taskRepository.save(listTask4);
     }
 }
